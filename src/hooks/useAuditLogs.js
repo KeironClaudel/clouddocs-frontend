37b55ac558e7ecfe72e6ca3d357526c5a1239adf
Keiron Claudel <keironqc@gmail.com>
2026-04-18 17:28:07 -0600

    fix: updated error handler import in useAuditLogs

--- src/hooks/useAuditLogs.js
+++ src/hooks/useAuditLogs.js
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getAuditLogs } from "../services/auditService";
-import { getApiErrorMessage } from "../utils/errorUtils";
+import { getApiErrorMessage } from "../utils/apiErrorHandler";
 import {
   buildAuditLogsParams,
   getInitialAuditFilters,
