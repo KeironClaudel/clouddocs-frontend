ae2b1e4b6989b4c871441781771f204dce00a4d2
Keiron Claudel <keironqc@gmail.com>
2026-04-18 17:29:50 -0600

    fix: deleted duplicate error handler import in useAuditLogs

--- src/hooks/useAuditLogs.js
+++ src/hooks/useAuditLogs.js
@@ -1,7 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { getAuditLogs } from "../services/auditService";
-import { resolveApiErrorMessage } from "../utils/apiErrorHandler";
 import {
   buildAuditLogsParams,
   getInitialAuditFilters,
